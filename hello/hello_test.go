8204d37b3364ec7f88fe8ef2fd124feb65daa629
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-11-18 08:47:59 -0300

    teste falhando

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -10,3 +10,10 @@ func TestHelloGitHub(t *testing.T) {
 		t.Errorf("HelloGitHub() = %s; want Hello GitHub", result)
 	}
 }
+
+func TestHelloFail(t *testing.T) {
+	result := HelloGitHub()
+	if result != "Fail" {
+		t.Errorf("HelloGitHub() = %s; want Hello GitHub", result)
+	}
+}
