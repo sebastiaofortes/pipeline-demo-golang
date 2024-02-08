56719a014bc85d79db25e048bc7c07368ba9b09f
Sebastião Fortes <sebastiaofortes4@gmail.com>
2024-02-07 21:12:34 -0300

    create server

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/sebastiaofortes/pipeline-demo-golang/hello"
 )
 
 func main()  {
 	// comment
 	fmt.Println(hello.HelloGitHub())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
