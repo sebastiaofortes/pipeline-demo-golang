869282fdba49171f44d6be5a012efc8b4c9888b4
Sebastião Fortes <sebastiaofortes4@gmail.com>
2024-02-11 22:40:07 -0300

    [Docker] new deploy (#11)
    * new deploy
    
    * create server
    
    * create server

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/sebastiaofortes/pipeline-demo-golang/hello"
 )
 
 func main()  {
+	// comment
 	fmt.Println(hello.HelloGitHub())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World"))
+		if err != nil{
+			fmt.Println(err)
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil{
+		fmt.Println(err)
+	}
 }
